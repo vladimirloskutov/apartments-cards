4135fa190f3b9242669e3a1251dc38d66b12ea6c
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-10-01 15:59:50 +0300

    add Provider to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import reducer from './reducers';
 
@@ -37,6 +38,8 @@ const entities = [
 const store = createStore(reducer, entities);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
